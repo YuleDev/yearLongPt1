6c3452222119b42fd5bb916a9978fa773b79ac71
kole ervine <koleervine@gmail.com>
2023-02-22 09:43:20 -0700

    added white behind labels on settings while in darkmode

--- YearLongPt1/YearLongPt1/ViewController.swift
+++ YearLongPt1/YearLongPt1/ViewController.swift
@@ -2,7 +2,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    @IBOutlet var lightModeButton: UILabel!
+    @IBOutlet var darkModeButton: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -12,7 +14,7 @@ class ViewController: UIViewController {
     @IBAction func darkModeSwitch(_ sender: UISwitch) {
         // ternary op to change bg color? true or false ?? 1 : 2
         view.backgroundColor = sender.isOn ?  .white : .black
+        lightModeButton.backgroundColor = sender.isOn ? .clear : .white
+        darkModeButton.backgroundColor = sender.isOn ? .clear : .white
+        }
     }
-    
-}
-
